44dea5f0bc7010e52195497ce7fdf3edf487edc6
Abanoub1009 <bibosoliman20@icloud.com>
2024-12-02 23:08:52 +0200

    initialize and file Manager

---
+++ src/connect_hub/Connect_Hub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package connect_hub;
+
+/**
+ *
+ * @author bibos_bz87qw5
+ */
+public class Connect_Hub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
